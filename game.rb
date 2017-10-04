ad6151ddf1f93ca22e527d7a4a8ff9207a45ddec
anjarupnik <anjarupnik@hotmail.com>
2017-10-04 17:31:18 +0200

    No cnt game.rb

--- game.rb
+++ game.rb
@@ -1,11 +1,12 @@
 secret_num = 1 + rand(100)
-cnt = 0
 guess = 0
 points = 100
 
-while cnt<10
-puts "Make a guess?"
-guess = gets.chomp.to_i
+puts "Guess a number between 1-100!!\n"
+
+while points>0
+  puts "Make a guess?"
+  guess = gets.chomp.to_i
 if guess == secret_num
   puts "You won \nPoints: #{points}"
   break
@@ -14,7 +15,6 @@ elsif guess < secret_num
 else
   puts "Lower!!"
 end
-cnt  +=1
 points -= 10
-puts "Game over!! Computer wins\nPoints: #{points}" if cnt==10
+puts "Game over!! Computer wins\nPoints: #{points}" if points==0
 end
